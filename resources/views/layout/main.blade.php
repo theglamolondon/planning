0f2455b339f0f1d38cc62e7d268f96521bf5c0bb
Darnels <akejeansidoine@yahoo.fr>
2018-10-16 10:12:44 +0000

    mise en forme KPMG

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -272,10 +272,8 @@
             <footer class="footer">
                 <div class="container-fluid clearfix">
             <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2018
-              <a href="#" target="_blank">{{ env("APP_NAME") }}</a>. All rights reserved.</span>
-                    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with
-              <i class="mdi mdi-heart text-danger"></i>
-            </span>
+               All rights reserved.</span>
+                    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> </span>
                 </div>
             </footer>
             <!-- partial -->
